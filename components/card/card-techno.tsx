f911ee769dffc0c0060cb4f585cfb435c6a675d1
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-06-10 14:42:39 +0300

    change nextjs height

--- components/card/card-techno.tsx
+++ components/card/card-techno.tsx
@@ -6,13 +6,13 @@ import { Star } from 'lucide-react'
 
 const CardTechnologie: React.FC<Techno> = ({ id, name, language, type, icon, star }) => {
     return (
-        <Card key={id}>
+        <Card key={id} className='dark:bg-white'>
             <CardContent className='flex gap-2 m-2'>
-                <Image src={icon} alt={name} height={35} width={40} />
+                <Image  src={icon} alt={name} height={40} width={40} />
                 <div className='block mt-2'>
-                    <h4 className='text-md font-bold'>{name}</h4>
-                    {/* <h4 className='text-sm font-semibold'>{language ? language : ""}</h4> */}
-                    <h4 className='text-sm font-thin'>{type ? type : ""}</h4>
+                    <h4 className='text-md font-bold dark:text-secondary'>{name}</h4>
+                    <h4 className='text-sm font-normal dark:text-secondary'>{language ? language : ""}</h4>
+                    <h4 className='text-sm font-thin dark:text-secondary'>{type ? type : ""}</h4>
                 </div>
             </CardContent>
             <CardFooter className='flex gap-2'>
