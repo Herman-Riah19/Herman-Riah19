06059543d8dd4ec21bce699ad0e9978cb7be3c3e
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-08-24 09:05:18 +0300

    change the theme color

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -11,7 +11,7 @@ const badgeVariants = cva(
         default:
           "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
         secondary:
-          "border-transparent bg-secondary-foreground/80 text-white ",
+          "border-transparent bg-secondary/80 text-white dark:text-black",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
         outline: "text-foreground",
