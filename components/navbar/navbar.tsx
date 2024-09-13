23255a720eef16c27bfa63ff63c3f2afee8ba143
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-09-13 09:14:37 +0300

    delete unused program

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -110,32 +110,6 @@ const Navbar: React.FC = () => {
         </DockIcon>
       </Dock>
     </div>
-    // <header className="sticky top-0 flex h-16 z-10 items-center gap-1 border-b bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 md:px-6">
-    //   <Link
-    //     href="/"
-    //     className="flex items-center gap-6 text-lg font-semibold md:text-base"
-    //   >
-    //     <span className="font-semibold">Midas</span>
-    //   </Link>
-    //   <NavbarSheet pages={pages} />
-
-    //   <nav className="flex flex-col text-lg font-medium sm:hidden md:flex justify-end md:flex-row md:items-center md:gap-3 md:text-sm lg:gap-6">
-    //     {pages.map((page) => (
-    //       <Link
-    //         key={page.name}
-    //         href={page.link}
-    //         className="text-muted-foreground transition-colors hover:text-foreground w-[8vw]"
-    //       >
-    //         {page.name}
-    //       </Link>
-    //     ))}
-    //   </nav>
-
-    //   <div className="flex w-full justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
-    //     <NavLangue />
-    //     <DarkTheme />
-    //   </div>
-    // </header>
   );
 };
 
