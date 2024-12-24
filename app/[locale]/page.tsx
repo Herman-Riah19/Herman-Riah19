b28c67627a12554631194d9f7f57d343a44e7af6
Hermann <65443014+Herman-Riah19@users.noreply.github.com>
2024-12-24 08:53:57 +0300

    delete the animatedDemo

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -24,7 +24,7 @@ const HermanPage = () => {
         <Resume />
         <Education />
         <ListTech />
-        <AnimatedListDemo />
+        {/* <AnimatedListDemo /> */}
         <Projects />
         <HobieProject />
       </section>
