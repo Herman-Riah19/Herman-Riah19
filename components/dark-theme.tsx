bdcfc64c8559ff80d3a45bb3cd2444b27252fb79
Hermann <hermanrazafinbdranaivo@gmail.com>
2025-03-13 18:24:37 +0300

    change the icon into the darktheme

--- components/dark-theme.tsx
+++ components/dark-theme.tsx
@@ -17,8 +17,8 @@ export default function DarkTheme() {
             className="px-2"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
-            <Sun className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
-            <Moon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+            <Sun className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+            <Moon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
         </Button>
     )
 }
