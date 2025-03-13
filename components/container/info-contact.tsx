554d585758b7ad9aeb53b86f393adc66e3dc2663
Hermann <hermanrazafinbdranaivo@gmail.com>
2025-03-13 18:43:11 +0300

    change the twiter contact into linkeding

--- components/container/info-contact.tsx
+++ components/container/info-contact.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react';
-import { Github, Twitter, Instagram, FacebookIcon } from 'lucide-react';
+import { Github, FacebookIcon, Linkedin } from 'lucide-react';
 import Link from 'next/link';
 
 const InfoContact = () => {
@@ -15,9 +15,9 @@ const InfoContact = () => {
                     </Link>
                 </li>
                 <li >
-                    <Link href="https://x.com/HermannRiah" className='w-full rounded-md flex flex-row p-0 bg-secondary border-none gap-2 text-start text-white dark:text-black'>
-                        <Twitter className='m-2' />
-                        <span className='sm:text-md md:text-lg text-normal mt-2'>@hermannriah</span>
+                    <Link href="https://www.linkedin.com/in/hermann-razafindranaivo-174378237/" className='w-full rounded-md flex flex-row p-0 bg-secondary border-none gap-2 text-start text-white dark:text-black'>
+                        <Linkedin className='m-2' />
+                        <span className='sm:text-md md:text-lg text-normal mt-2'>Hermann Razafindranaivo</span>
                     </Link>
                 </li>
                 <li>
