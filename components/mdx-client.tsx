7386f488a67c7eee2da7ed1a04ac14708b6449ae
herman-irok <herman@irok.fr>
2025-09-06 19:06:23 +0300

    delete unused component

--- components/mdx-client.tsx
+++ components/mdx-client.tsx
@@ -13,34 +13,5 @@ export function MDXClientProvider({ children }: { children: React.ReactNode }) {
   );
 }
 
-export function MdxContent({ mdxString }: { mdxString: string }) {
-  const [Component, setComponent] = useState<React.ComponentType | null>(null);
-
-  useEffect(() => {
-    async function compileMdx() {
-      // Compiler le MDX en code JSX
-      const compiled = await compile(mdxString, {
-        outputFormat: "function-body",
-        useDynamicImport: false,
-      });
-
-      // Convertir le code compilé en fonction React
-      const fn = new Function(
-        "React",
-        "MDXProvider",
-        "components",
-        `${compiled}`
-      );
-
-      setComponent(() => () => fn(React, MDXProvider, useMDXComponents({})));
-    }
-
-    compileMdx();
-  }, [mdxString]);
-
-  if (!Component) return <p>Chargement...</p>;
-
-  return <Component />;
-}
 
 
