2a34fa0a0751fb6d97e730026f1aa05b0e2c9577
herman-riah <hermanrazafinbdranaivo@gmail.com>
2025-09-15 10:27:42 +0300

    fix: adding new project

--- model/ProjectModel.ts
+++ model/ProjectModel.ts
@@ -1,52 +1,73 @@
-
 export interface Model {
-    id: number;
-    href: string;
-    title: string;
-    type: string;
-    img: string;
-    link: string;
-    description: string;
-    date: string;
-    technology: string[];
+  id: number;
+  href: string;
+  title: string;
+  type: string;
+  img: string;
+  link: string;
+  description: string;
+  date: string;
+  technology: string[];
 }
 
 export let ProjectModelFr: Array<Model> = [
   {
     id: 1,
+    href: "https://web-companies-two.vercel.app/",
+    title: "Web Ltd",
+    type: "Site vitrine / Landing page",
+    img: "/assets/image/web_compagnie.png",
+    link: "",
+    description:
+      "Développement et intégration du site vitrine de Web Compagnie France, filiale de l'agence d'origine mauricienne, marquant son implantation sur le marché français.",
+    date: "Juillet 2025 - Août 2025",
+    technology: ["Next.js", "React.js", "Shadcn UI", "Drupal"],
+  },
+  {
+    id: 2,
     href: "https://riah-market.vercel.app/fr",
     title: "Riah market",
     type: "NFT market powered by AI",
     img: `/assets/image/riah-market.png`,
     link: "",
-    description: "Riah market est une plateforme de marché NFT alimentée par l'IA, permettant aux utilisateurs d'explorer et d'acheter des œuvres d'art numériques uniques.",
+    description:
+      "Riah market est une plateforme de marché NFT alimentée par l'IA, permettant aux utilisateurs d'explorer et d'acheter des œuvres d'art numériques uniques.",
     date: "Mai 2025 - Juin 2025",
-    technology: ["Next.js", "React.js", "Shadcn UI", "PostgreSQL", "Hardhat", "Solidity"],
+    technology: [
+      "Next.js",
+      "React.js",
+      "Shadcn UI",
+      "PostgreSQL",
+      "Hardhat",
+      "Solidity",
+    ],
   },
   {
-    id: 2,
+    id: 3,
     href: "https://didascale.vercel.app/fr",
     title: "Didache",
     type: "IA pour Encyclopedie Eglise",
     img: `/assets/image/didache.PNG`,
     link: "",
-    description: "Didache est une application de chat immersive et multi-utilisateurs alimentée par l'IA qui explore les Pères de l'Église, les personnalités chrétiennes, les conciles, les événements chrétiens.",
+    description:
+      "Didache est une application de chat immersive et multi-utilisateurs alimentée par l'IA qui explore les Pères de l'Église, les personnalités chrétiennes, les conciles, les événements chrétiens.",
     date: "Janvier 2025 - Février 2025",
     technology: ["NextJS", "ReactJs", "Shadcn UI", "Firebase", "Mapbox"],
   },
   {
-    id: 3,
+    id: 4,
     href: "https://open-myst-web-app.vercel.app/",
     title: "OpenMyst",
     type: "Similateur 3D",
     img: `/assets/image/openmyst.PNG`,
     link: "",
-    description: "Open Myst est une plateforme de visualisation 3D qui rassemble les données les plus fiables disponibles sur la vie de Jésus.",
+    description:
+      "Open Myst est une plateforme de visualisation 3D qui rassemble les données les plus fiables disponibles sur la vie de Jésus.",
     date: "Mai 2024 - Décembre 2024",
     technology: ["NextJS", "ReactJs", "Shadcn UI", "Firebase", "Mapbox"],
   },
   {
-    id: 4,
+    id: 5,
     href: "https://midas-stability.vercel.app/fr",
     title: "Image Upscaler",
     type: "Generateur image",
@@ -58,35 +79,38 @@ export let ProjectModelFr: Array<Model> = [
     technology: ["Flask", "ReactJs", "Shadcn UI", "Prisma", "Postgre SQL"],
   },
   {
-    id: 5,
+    id: 6,
     href: "http://jupiter.fr",
     title: "Jupiter",
     type: "Application de Crowfunding",
     img: `/assets/image/Jupiter.png`,
     link: "",
-    description: "C'est un application web qui sert à achéter/transferer des forfaits et aussi les transferts d'argent avec les Mobile Money",
+    description:
+      "C'est un application web qui sert à achéter/transferer des forfaits et aussi les transferts d'argent avec les Mobile Money",
     date: "Juillet 2023 - Aout 2023",
     technology: ["Flask", "ReactJs", "MaterialUI", "Firebase"],
   },
   {
-    id: 6,
+    id: 7,
     href: "https://webmaster-landing.vercel.app",
     title: "Webmaster",
     type: "Landing page",
     img: `/assets/image/webmaster.PNG`,
     link: "",
-    description: "Un page d'accueil pour une entreprise qui vise à fournir une plateforme efficace et optimisée permettant à l’entreprise de gérer ses prestations, d’attirer de nouveaux clients et d’améliorer son processus de développement.",
+    description:
+      "Un page d'accueil pour une entreprise qui vise à fournir une plateforme efficace et optimisée permettant à l’entreprise de gérer ses prestations, d’attirer de nouveaux clients et d’améliorer son processus de développement.",
     date: "Août 2024",
     technology: ["Next JS", "ReactJS", "Shadcn UI", "Tailwind"],
   },
   {
-    id: 7,
+    id: 8,
     href: "https://github.com/Herman-Riah19/TryInc",
     title: "TryInk",
     type: "Réseau Social",
     img: `/assets/image/trink.png`,
     link: "https://github.com/Herman-Riah19/TryInc",
-    description: "Cette application est un réseau social destiné aux dessinateurs numériques et générateurs d'images par IA, mettant en avant leur talent.",
+    description:
+      "Cette application est un réseau social destiné aux dessinateurs numériques et générateurs d'images par IA, mettant en avant leur talent.",
     date: "Septembre 2022 - Mai 2023",
     technology: ["AdonisJS", "ReactJS", "MaterialUI", "Sqlite"],
   },
@@ -95,39 +119,60 @@ export let ProjectModelFr: Array<Model> = [
 export const ProjectModelEn: Array<Model> = [
   {
     id: 1,
+    href: "https://web-companies-two.vercel.app/",
+    title: "Web Ltd",
+    type: "Site vitrine / Landing page",
+    img: "/assets/image/web_compagnie.png",
+    link: "",
+    description: "Development and integration of the showcase website for Web Compagnie France, a subsidiary of the original Mauritius-based agency, marking its entry into the French market.",
+    date: "July 2025 - August 2025",
+    technology: ["Next.js", "React.js", "Shadcn UI", "Drupal"],
+  },
+  {
+    id: 2,
     href: "https://riah-market.vercel.app/fr",
     title: "Riah market",
     type: "NFT market powered by AI",
     img: `/assets/image/riah-market.png`,
     link: "",
-    description: "Riah market is an AI-powered NFT marketplace platform, allowing users to explore and purchase unique digital artworks.",
+    description:
+      "Riah market is an AI-powered NFT marketplace platform, allowing users to explore and purchase unique digital artworks.",
     date: "May 2025 - June 2025",
-    technology: ["Next.js", "React.js", "Shadcn UI", "PostgreSQL", "Hardhat", "Solidity"],
+    technology: [
+      "Next.js",
+      "React.js",
+      "Shadcn UI",
+      "PostgreSQL",
+      "Hardhat",
+      "Solidity",
+    ],
   },
   {
-    id: 2,
+    id: 3,
     href: "https://didascale.vercel.app/fr",
     title: "Didache",
     type: "AI for Church Encyclopedia",
     img: `/assets/image/didache.PNG`,
     link: "",
-    description: "Didache is an immersive, multi-user chat application powered by AI that explores the Church Fathers, Christian figures, councils, and Christian events.",
+    description:
+      "Didache is an immersive, multi-user chat application powered by AI that explores the Church Fathers, Christian figures, councils, and Christian events.",
     date: "January 2025 - February 2025",
     technology: ["NextJS", "ReactJs", "Shadcn UI", "Firebase", "Mapbox"],
   },
   {
-    id: 3,
+    id: 4,
     href: "https://open-myst-web-app.vercel.app/",
     title: "OpenMyst",
     type: "3D Simulator",
     img: `/assets/image/openmyst.PNG`,
     link: "",
-    description: "Open Myst is a 3D visualization platform that brings together the most reliable data available on the life of Jesus.",
+    description:
+      "Open Myst is a 3D visualization platform that brings together the most reliable data available on the life of Jesus.",
     date: "May 2024 - December 2024",
     technology: ["NextJS", "ReactJs", "Shadcn UI", "Firebase", "Mapbox"],
   },
   {
-    id: 4,
+    id: 5,
     href: "https://midas-stability.vercel.app/fr",
     title: "Image Upscaler",
     type: "Image Generator",
@@ -139,35 +184,38 @@ export const ProjectModelEn: Array<Model> = [
     technology: ["Flask", "ReactJs", "Shadcn UI", "Prisma", "Postgre SQL"],
   },
   {
-    id: 5,
+    id: 6,
     href: "http://jupiter.fr",
     title: "Jupiter",
     type: "Crowdfunding Application",
     img: `/assets/image/Jupiter.png`,
     link: "",
-    description: "This is a web application for buying/transferring plans and also for transferring money using Mobile Money.",
+    description:
+      "This is a web application for buying/transferring plans and also for transferring money using Mobile Money.",
     date: "July 2023 - August 2023",
     technology: ["Flask", "ReactJs", "MaterialUI", "Firebase"],
   },
   {
-    id: 6,
+    id: 7,
     href: "https://webmaster-landing.vercel.app",
     title: "Webmaster",
     type: "Landing page",
     img: `/assets/image/webmaster.PNG`,
     link: "",
-    description: "A landing page for a company that aims to provide an efficient and optimized platform for the company to manage its services, attract new clients, and improve its development process.",
+    description:
+      "A landing page for a company that aims to provide an efficient and optimized platform for the company to manage its services, attract new clients, and improve its development process.",
     date: "August 2024",
     technology: ["Next JS", "ReactJS", "Shadcn UI", "Tailwind"],
   },
   {
-    id: 7,
+    id: 8,
     href: "https://github.com/Herman-Riah19/TryInc",
     title: "TryInk",
     type: "Social Network",
     img: `/assets/image/trink.png`,
     link: "https://github.com/Herman-Riah19/TryInc",
-    description: "This application is a social network designed for digital illustrators and AI image generators, showcasing their talent.",
+    description:
+      "This application is a social network designed for digital illustrators and AI image generators, showcasing their talent.",
     date: "September 2022 - May 2023",
     technology: ["AdonisJS", "ReactJS", "MaterialUI", "Sqlite"],
   },
