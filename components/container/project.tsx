b682aae56bfe8d7bd404549d90a119b05432c318
herman-riah <hermanrazafinbdranaivo@gmail.com>
2025-09-15 12:00:31 +0300

    fix: change the introduction text

--- components/container/project.tsx
+++ components/container/project.tsx
@@ -61,7 +61,7 @@ export const Projects = () => {
           ))}
         </div>
         <Link
-          href={`/${local}/about`}
+          href={`/${local}/projects`}
           className="group relative mx-auto flex items-center justify-center rounded-full px-4 py-1.5 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] "
         >
           <span
