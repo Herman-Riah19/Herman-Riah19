f8a5b12edd7071769617954cd3a36c410da4e2b9
Riah19 <65443014+Herman-Riah19@users.noreply.github.com>
2026-03-23 13:32:37 +0200

    Update layout.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
 import "@/styles/globals.css";
+
+export const metadata = {
+  verification: {
+    google: "9uYJjiHuL-D2M9ZwPQ9_FTOZDSnbXrYcHhno-KNQm7c"
+  }
+}
 export default function RootLayout({
   children,
 }: Readonly<{
